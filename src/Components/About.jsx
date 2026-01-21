3e05732ba1caa03733c4e47d04caa0cb6d6118af
khushpatel00 <patelxkhush@gmail.com>
2026-01-21 21:06:01 +0530

    update: replaced Marquee text to TextPressure

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,17 +3,24 @@ import React from 'react'
 
 function About() {
     return (
-        <div className='bg-[#CDEA68] rounded-2xl py-20 pe-30'>
-            <p className='text-7xl text-zinc-800 py-20 px-16 neue'>We craft category-defining presentations, brand identities, and digital experiences that drive funding, sales, and market leadership.</p>
+        <div className='bg-[#CDEA68] rounded-2xl flex justify-between items-center py-20 '>
 
-            <div className='border-t border-black/30 px-16'>
-                <p className='py-10  neue text-7xl text-zinc-800'>How we can help:</p>
-                <button className='flex items-center gap-3 justify-between bg-zinc-800 text-zinc-200 uppercase text-lg neue rounded-full ps-7 pe-3 py-3'>
-                    Read More
-                    <p><ArrowUpRightIcon className='bg-white text-black w-16 h-16 rounded-full ms-10 p-5' /></p>
-                </button>
+            <div>
+                <p className='text-7xl text-zinc-800 py-20 px-16 neue pe-30'>We craft category-defining presentations, brand identities, and digital experiences that drive funding, sales, and market leadership.</p>
 
+                <div className='border-t flex justify-between py-10 border-black/30 px-16'>
+                    <div>
+                        <p className='py-10  neue text-7xl text-zinc-800'>How we can help:</p>
+                        <button className='flex items-center gap-3 justify-between bg-zinc-800 text-zinc-200 uppercase text-lg neue rounded-full ps-7 pe-3 py-3'>
+                            Read More
+                            <p><ArrowUpRightIcon className='bg-white text-black w-16 h-16 rounded-full ms-10 p-5' /></p>
+                        </button>
+                    </div>
+                    <img src="/images/Homepage-Photo-663x469.jpg" className='basis-1/2 rounded-2xl w-full h-auto ' alt="" />
+
+                </div>
             </div>
+
 
         </div>
     )
--- src/Components/Marquee.jsx
+++ src/Components/Marquee.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import TextPressure from './TextPressure';
+
 
 function Marquee() {
   return (
     <div>
-        <div className="w-screen overflow-hidden rounded-2xl bg-[#004d43] py-10 flex">
-            <div className='w-full border-t-white/30 h-1 border-t translate-y-18 duration-300'></div>
-            <div className="text-[580px] uppercase text-nowrap leading-[480px] text-white font-[Founders_Grotesk_Condensed] font-bold">We are Ochi</div>
-            <div className="text-[580px] uppercase text-nowrap leading-[480px] text-white font-[Founders_Grotesk_Condensed] font-bold">We are Ochi</div>
+      <div className="w-screen rounded-2xl bg-[#004d43] min-h-[60vh] max-h-[70vh] py-10">
+        <div className='w-full border-t-white/30 h-1 border-t translate-y-12 duration-300'></div>
+        {/* <div className="text-[580px] uppercase text-nowrap leading-120 text-white font-[Founders_Grotesk_Condensed] font-bold">We are Ochi</div>
+        <div className="text-[580px] uppercase text-nowrap leading-120 text-white font-[Founders_Grotesk_Condensed] font-bold">We are Ochi</div> */}
+
+        <div className='' style={{ position: 'relative', height: '300px' }}>
+          <TextPressure
+            text="Weare Ochi"
+            flex
+            stroke={false}
+            width
+            weight
+            italic
+            textColor="#ffffff"
+            strokeColor="#5227FF"
+            minFontSize={36}
+          />
         </div>
+
+      </div>
     </div>
   )
 }
